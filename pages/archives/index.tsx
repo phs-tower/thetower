64fb2c92c465b85928f753e6f7f2a7442f077bd5
singla-aryan <singlaaryan08@gmail.com>
2025-05-04 17:49:56 -0400

    added 2025 archive

--- pages/archives/index.tsx
+++ pages/archives/index.tsx
@@ -38,6 +38,22 @@ export default function Archives() {
 			`}</style>
 			<h1>Archives</h1>
 			<br></br>
+
+			<h2>2025</h2>
+			<div className="container">
+				<VirtualArchive month={2} year={2025} />
+				<VirtualArchive month={3} year={2025} />
+				{/*
+
+				<VirtualArchive month={4} year={2025} />
+				<VirtualArchive month={6} year={2025} />
+				<VirtualArchive month={9} year={2025} />
+				<VirtualArchive month={10} year={2025} />
+				<VirtualArchive month={11} year={2025} />
+				<VirtualArchive month={12} year={2025} /> 
+				*/}
+			</div>
+
 			<h2>2024</h2>
 			<div className="container">
 				<VirtualArchive month={2} year={2024} />
